acf0841fdd4dcc53c40be7d15325a92c32e3a1e8
Chanchal Tiwari <chanchaltiwari448@gmail.com>
2022-07-25 17:22:12 +0530

    trying to my second commit

---
+++ src/test/java/gitprac/GitBashGitHubPrac.java
@@ -0,0 +1,13 @@
+package gitprac;
+
+public class GitBashGitHubPrac 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("trying to push my first code");
+		System.out.println("trying to push my second line code");
+
+	}
+
+}
